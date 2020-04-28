9a7695dc50941cb5bd82644d3559bde6d24f7de8
rsnazario <rafael.s.nazario@gmail.com>
2020-04-28 17:04:45 -0300

    Applied Step I1 from the Blog App Tutorial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,12 +16,15 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])      
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_path(@articles)
   end
 
@@ -30,6 +33,12 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
     redirect_to article_path(@article)
   end
+
 end
